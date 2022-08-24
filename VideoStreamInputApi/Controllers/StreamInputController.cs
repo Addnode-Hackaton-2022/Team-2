c842e9849c764ed363906a1ea890b58f35fdfaf1
fredrikaslin <fredrikaslin@hotmail.com>
2022-08-24 14:33:37 +0200

    init

---
+++ VideoStreamInputApi/Controllers/StreamInputController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RtspClientSharp;
+using RtspClientSharp.RawFrames.Audio;
+using RtspClientSharp.RawFrames.Video;
+using RtspClientSharp.Rtsp;
+using System.Net;
+
+namespace VideoStreamInputApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StreamInputController : ControllerBase
+    {
+        [HttpGet(Name = "Start")]
+        public async Task<IActionResult> Get()
+        {
+            var serverUri = new Uri("rtsp://10.0.2.100:8554/rpicamuni");
+            var credentials = new NetworkCredential("admin", "hackaddthon");
+
+            var connectionParameters = new ConnectionParameters(serverUri, credentials);
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            Task connectTask = ConnectAsync(connectionParameters, cancellationTokenSource.Token);
+
+            Console.WriteLine("Press any key to cancel");
+            Console.ReadLine();
+
+            cancellationTokenSource.Cancel();
+
+            Console.WriteLine("Canceling");
+            connectTask.Wait(CancellationToken.None);
+
+            return Ok(connectionParameters);
+        }
+
+        private static async Task ConnectAsync(ConnectionParameters connectionParameters, CancellationToken token)
+        {
+            try
+            {
+                TimeSpan delay = TimeSpan.FromSeconds(5);
+
+                using (var rtspClient = new RtspClient(connectionParameters))
+                {
+                    rtspClient.FrameReceived +=
+                        (sender, frame) => Console.WriteLine($"New frame {frame.Timestamp}: {frame.GetType().Name}");
+
+                    while (true)
+                    {
+                        Console.WriteLine("Connecting...");
+
+                        try
+                        {
+                            await rtspClient.ConnectAsync(token);
+                        }
+                        catch (OperationCanceledException)
+                        {
+                            return;
+                        }
+                        catch (RtspClientException e)
+                        {
+                            Console.WriteLine(e.ToString());
+                            await Task.Delay(delay, token);
+                            continue;
+                        }
+
+                        Console.WriteLine("Connected.");
+
+                        try
+                        {
+                            await rtspClient.ReceiveAsync(token);
+                        }
+                        catch (OperationCanceledException)
+                        {
+                            return;
+                        }
+                        catch (RtspClientException e)
+                        {
+                            Console.WriteLine(e.ToString());
+                            await Task.Delay(delay, token);
+                        }
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+            }
+        }
+    }
+}
